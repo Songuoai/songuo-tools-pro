00660ce2f1d684a35d4616cd6d3b46b73b90cf88
朱永松 <1335979521@qq.com>
2026-04-02 17:25:15 +0800

    触发自动部署

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const nextConfig = {
   },
   reactStrictMode: false,
   poweredByHeader: false,
+  // 禁用静态生成，使用 SSR
+  experimental: {
+    serverComponents: true,
+  },
 }
 
 module.exports = nextConfig
